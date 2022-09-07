f932caf8956fcbc1e6d67f9b741fac9ca86f3a9d
rivervil16 <agustinriverovila@gmail.com>
2022-09-07 18:15:25 +0000

    04 Conditional Rendering

--- .learn/resets/03.4-jumbotron/app.jsx
+++ .learn/resets/03.4-jumbotron/app.jsx
@@ -2,25 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
-const Jumbotron = (props) => {
+const Jumbotron = props => {
 	//here you have to return expected html using the properties being passed to the component
-	return (
-		<div className="jumbotron m-5">
-			<h1 className="display-4">{props.title}</h1>
-			<p className="lead">{props.description}</p>
-			<a className="btn btn-primary btn-lg" href={props.buttonURL} role="button">
-				{props.buttonLabel}
-			</a>
-		</div>
-	);
 };
 
 Jumbotron.propTypes = {
 	//proptypes here
 	title: PropTypes.string,
-	description: PropTypes.string,
-	buttonLabel: PropTypes.string,
-	buttonURL: PropTypes.string,
+
 };
 
 ReactDOM.render(
@@ -28,7 +17,7 @@ ReactDOM.render(
 		title="Welcome to react"
 		description="React is the most popular rendering library in the world"
 		buttonLabel="Go to the official website"
-		buttonURL={"https://reactjs.org/"}
+		buttonURL="https://reactjs.org/"
 	/>,
 
 	document.querySelector("#myDiv")
